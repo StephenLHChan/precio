359999f61ec5cdf3328a7890097f6bf845859175
StephenLHChan <stephenc.1018@gmail.com>
2023-07-09 21:14:23 -0400

    feat(auth): :sparkles: protect routes

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -2,19 +2,28 @@
 
 import Button from '@/app/components/Button';
 import Input from '@/app/components/inputs/Input';
-import { useState, useCallback } from 'react';
-import { signIn } from 'next-auth/react';
+import { useState, useCallback, useEffect } from 'react';
+import { signIn, useSession } from 'next-auth/react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import AuthSocialButton from './AuthSocialButton';
 import { BsFacebook, BsGoogle, BsInstagram } from 'react-icons/bs';
 import axios from 'axios';
 import { toast } from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
 
 type Variant = 'LOGIN' | 'REGISTER';
 
 const AuthForm = () => {
+	const session = useSession();
+	const router = useRouter();
 	const [variant, setVariant] = useState<Variant>('LOGIN');
 	const [isLoading, setIsLoading] = useState<boolean>(false);
+
+	useEffect(() => {
+		if (session?.status === 'authenticated') {
+			router.push('/home');
+		}
+	}, [session?.status, router]);
 
 	const toggleVariant = useCallback(() => {
 		if (variant === 'LOGIN') {
@@ -42,6 +51,7 @@ const AuthForm = () => {
 		if (variant === 'REGISTER') {
 			axios
 				.post('/api/register', data)
+				.then(() => signIn('credentials', data))
 				.catch(() => toast.error('Something went wrong!'))
 				.finally(() => setIsLoading(false));
 		}
@@ -55,6 +65,7 @@ const AuthForm = () => {
 
 					if (callback?.ok && !callback?.error) {
 						toast.success('Logged in');
+						router.push('/home');
 					}
 				})
 				.finally(() => setIsLoading(false));
@@ -63,6 +74,7 @@ const AuthForm = () => {
 
 	const socialAction = (action: string) => {
 		setIsLoading(true);
+
 		signIn(action, { redirect: false })
 			.then((callback) => {
 				if (callback?.error) {
@@ -71,6 +83,7 @@ const AuthForm = () => {
 
 				if (callback?.ok && !callback?.error) {
 					toast.success('Logged in');
+					router.push('/home');
 				}
 			})
 			.finally(() => setIsLoading(false));
